45dfd9e5ae717d99878bb474c3760ddb8d2713c3
Alexander Ananchenko <snvtr@mail.ru>
2019-03-18 16:21:23 +0300

    Add files via upload

---
+++ string_prefix.py
@@ -0,0 +1,23 @@
+def prefix(A):
+    """ calculates the string prefix """
+
+    Pi = [0] * (len(A))
+
+    for i in range(1,len(A)):
+
+        p = Pi[i-1]
+
+        while p > 0 and A[p] != A[i]:
+            p = Pi[p-1]
+
+        if A[p] == A[i]:
+            p += 1
+
+        Pi[i] = p
+
+    return Pi
+
+
+S = 'in@string'
+
+print(prefix(S))
