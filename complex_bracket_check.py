a2abdd0ff7a41dfcf10ff66cc157ef3cd00b2bb2
Alexander Ananchenko <snvtr@mail.ru>
2019-03-22 16:34:13 +0300

    лекция 13 - проверка разных типов скобок

--- complex_bracket_check.py
+++ complex_bracket_check.py
@@ -5,9 +5,35 @@ def complex_bracket_check(A):
 
         >>> complex_bracket_check('(])')
         'Err'
-        >>> complex_bracket_check('([])')
+        >>> complex_bracket_check('[[]]')
         'Ok'
+        >>> complex_bracket_check('(([(]))())')
+        'Err'
     """
+    cnt_r = 0
+    cnt_s = 0
+    prev = A[0]
+#    print('r: %i, s: %i' % (cnt_r, cnt_s))
+    for c in A:
+        if c == ')' and prev == '[': # некорректно
+            return 'Err'
+        if c == ']' and prev == '(': # некорректно
+            return 'Err'
+				
+        if c == '(':
+            cnt_r += 1
+        if c == '[':
+            cnt_s += 1
+        if c == ')':
+            cnt_r -= 1
+        if c == ']':
+            cnt_s -= 1
+
+        prev = c
+ #       print('r: %i, s: %i' % (cnt_r, cnt_s))
+
+    if cnt_r + cnt_s != 0:        # некорректно
+        return 'Err'
 
     return 'Ok'
 
@@ -15,3 +41,4 @@ def complex_bracket_check(A):
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+#    print(complex_bracket_check('(([(]))())'))
