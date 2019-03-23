5dedfed473a98df2691a02303ca90035926a211f
Alexander Ananchenko <snvtr@mail.ru>
2019-03-23 16:20:06 +0300

    doctest

--- reverse_notation.py
+++ reverse_notation.py
@@ -1,5 +1,7 @@
 import sys
 
+# 4doctest:
+
 class Stack():
     """
         реализует имитацию стека
@@ -32,23 +34,43 @@ def is_int(x):
     except:
         return False
 
+def doctest_stub_func():
+    """
+    >>> is_int(0)
+    True
+    >>> is_int('A')
+    False
+    >>> my_stack = Stack()
+    >>> my_stack.append(0)
+    >>> my_stack.append(1)
+    >>> my_stack.clear()
+    >>> my_stack.debug()
+    []
+    """
+    pass
+
 #A = [2,2,'+']
-#A = [2,3,4,'+','*'] (3+4)*2
-A = [1,2,3,'-',4,'+','*'] (3-2+4)*1
-my_stack = Stack()
+#A = [2,3,4,'+','*'] # (3+4)*2
 
-for c in A:
-    if c in ['+','-','*','/']: # basic arithmetic operands
-        x = my_stack.pop()     # pity, no checks for None
-        y = my_stack.pop()     # here either
-        if x is not None and y is not None:
-            cmd = str(x) + c + str(y)
-            val = eval(cmd) # I know, eval() is evil
-            my_stack.append(val)
-        else:
-            print('Something wrong with the stack')
-            sys.exit()
-    elif is_int(c):
-        my_stack.append(c)
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
-my_stack.debug()
+    A = [1,2,3,'-',4,'+','*'] # (3-2+4)*1
+    my_stack = Stack()
+
+    for c in A:
+        if c in ['+','-','*','/']: # basic arithmetic operands
+            x = my_stack.pop()     # pity, no checks for None
+            y = my_stack.pop()     # here either
+            if x is not None and y is not None:
+                cmd = str(x) + c + str(y)
+                val = eval(cmd) # I know, eval() is evil
+                my_stack.append(val)
+            else:
+                print('Something wrong with the stack')
+                sys.exit()
+        elif is_int(c):
+            my_stack.append(c)
+
+    my_stack.debug()
