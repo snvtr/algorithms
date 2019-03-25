c91d59cf51c01f4387cdde64969dff2a33cd3715
Alexander Ananchenko <snvtr@mail.ru>
2019-03-25 16:14:22 +0300

    скелеты для кучи

---
+++ class_heap.py
@@ -0,0 +1,21 @@
+
+
+class Heap():
+
+    def __init__(self):
+        self.size = 0
+        self.Heap = []
+
+    def add(self, new_val):
+        pass
+
+    def del(self, del_val):
+        pass
+
+    def swap_up(self, child):
+        pass
+
+    def swap_down(self, empty_idx):
+        pass
+
+heap = Heap()
---
+++ simple_heap.py
@@ -0,0 +1,22 @@
+
+### --- subs() --- ###
+
+def add(heap, new_val):
+    """ добавляет новый элемент в кучу """
+    pass
+
+def swap_up(heap, new_val):
+    """ меняет элементы местами с перемещением вверх """
+    pass
+
+def swap_down(heap, new_val):
+    """ меняет элементы местами с перемещением вниз """
+    pass
+
+def delete(heap, del_idx):
+    """ удаляет элемент из кучи """
+    pass
+
+### --- main() --- ###
+
+heap = []
