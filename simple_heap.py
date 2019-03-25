3e4f9d15e57348b1d4ca92a1555203121e665700
Alexander Ananchenko <snvtr@mail.ru>
2019-03-25 21:43:39 +0300

    simple heap

--- simple_heap.py
+++ simple_heap.py
@@ -20,20 +20,36 @@ def swap_up(heap, new_idx):
 
 def delete(heap, del_idx):
     """ удаляет элемент из кучи по индексу """
-    pass
+    swap_down(heap, del_idx)
+    x = heap.pop()
+
 
 def swap_down(heap, del_idx):
     """ меняет элементы местами с перемещением вниз """
-    pass
+    if del_idx >= len(heap) // 2:
+        return
+
+    if heap[del_idx*2] < heap[del_idx*2+1]:
+        heap[del_idx] = heap[del_idx*2]
+        swap_down(heap, del_idx*2)
+    else:
+        heap[del_idx] = heap[del_idx*2+1]
+        swap_down(heap, del_idx*2+1)
 
 ### --- main() --- ###
 
-my_heap = []
+my_heap = [-1]
 
+add(my_heap, 2)
+add(my_heap, 6)
 add(my_heap, 4)
 add(my_heap, 3)
-add(my_heap, 2)
+add(my_heap, 5)
 add(my_heap, 1)
 add(my_heap, 0)
+
+print(my_heap)
+
+delete(my_heap,3)
 
 print(my_heap)
